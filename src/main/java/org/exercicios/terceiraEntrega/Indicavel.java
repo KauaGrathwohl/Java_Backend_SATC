5e62a0ebbd37ced43e24c55bd2df39ccabc78711
Kauã Machado Grathwohl <kaua.grathwohl@thomsonreuters.com>
2024-03-29 12:32:28 -0300

    Oscar Service

---
+++ src/main/java/org/exercicios/terceiraEntrega/Indicavel.java
@@ -0,0 +1,11 @@
+package org.exercicios.terceiraEntrega;
+
+public interface Indicavel {
+
+    boolean isElegivel();
+
+    void setElegivel(boolean elegivel);
+
+    int getNumeroDeIndicacoes();
+    void incrementarIndicacoes();
+}
